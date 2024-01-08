1648acf650a952cff4656f087a7f24291e0bbc79
Susmita N.M. Bhattacharya <87614560+SB2318@users.noreply.github.com>
2024-01-08 23:18:36 +0530

    Create Sp-RangeSumBST-080124.java

---
+++ LeetCode/POTD/Sp-RangeSumBST-080124.java
@@ -0,0 +1,41 @@
+/***
+    Problem Date: 08/01/24
+    Problem Name:  Range Sum of BST
+    Problem Link: https://leetcode.com/problems/range-sum-of-bst/
+    Problem Category: BST, DSA
+  */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        
+if(root == null){
+    return 0;
+}
+
+else if(root.val >= low && root.val <= high){
+    return root.val + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low,high);
+}
+
+else if(root.val < low){
+    return rangeSumBST(root.right,low,high);
+}
+else 
+return rangeSumBST(root.left,low,high);
+}
+
+}
