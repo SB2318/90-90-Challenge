376d0c82a8fe01cfe0ad8db31636bb4980b207b4
Susmita N.M. Bhattacharya <87614560+SB2318@users.noreply.github.com>
2025-01-15 10:52:33 +0530

    Create EquilibriumPoint.java

---
+++ GFG/140125/Java/EquilibriumPoint.java
@@ -0,0 +1,39 @@
+// Problem Link: https://www.geeksforgeeks.org/problems/equilibrium-point-1587115620/1
+/**
+Intution: 
+According to the requirement, you have to check for each index, whether the left sum equal to the right sum or not.
+Here in the code, first calculate the total sum, 
+Then for each index, substract the current element, check if the left sum equal to right sum or not.
+
+Important Variables: 
+
+ls => This keeps track of the sum of elements to the left of the current index.
+rs => This keeps track of the sum of elements to the right of the current index.
+
+
+*/
+class Solution {
+    // Function to find equilibrium point in the array.
+    public static int findEquilibrium(int arr[]) {
+        // code here
+        
+        int ls=0, rs=0;
+        
+        for(int i=0; i<arr.length; i++){
+            rs+= arr[i];
+        }
+        
+        for(int i=0; i<arr.length; i++){
+            
+            rs-=arr[i];
+            
+            if(ls== rs){
+                return i;
+            }
+            
+            ls+= arr[i];
+        }
+        
+        return -1;
+    }
+}
