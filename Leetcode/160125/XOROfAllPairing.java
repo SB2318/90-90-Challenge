2334ee0596497fd697b555e45d5c92e1e308a1bd
Susmita N.M. Bhattacharya <87614560+SB2318@users.noreply.github.com>
2025-01-17 19:38:39 +0530

    Create XOROfAllPairing.java

---
+++ Leetcode/160125/XOROfAllPairing.java
@@ -0,0 +1,28 @@
+// Problem Link: https://leetcode.com/problems/bitwise-xor-of-all-pairings/editorial/?envType=daily-question&envId=2025-01-16
+// Solution Link: Problem Editorial (My Suggestion)
+class Solution {
+
+    public int xorAllNums(int[] nums1, int[] nums2) {
+        // Get lengths of arrays
+        int len1 = nums1.length;
+        int len2 = nums2.length;
+
+        Map<Integer, Integer> freq = new HashMap<>();
+
+        for (int num : nums1) {
+            freq.put(num, freq.getOrDefault(num, 0) + len2);
+        }
+        for (int num : nums2) {
+            freq.put(num, freq.getOrDefault(num, 0) + len1);
+        }
+
+        int ans = 0;
+        for (int num : freq.keySet()) {
+            if (freq.get(num) % 2 == 1) {
+                ans ^= num;
+            }
+        }
+
+        return ans;
+    }
+}
